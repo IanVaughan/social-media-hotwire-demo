1f8fa05d99d76bfb35b4b9c4ad5327db4eb66648
Ian <ianrvaughan@gmail.com>
2021-10-12 07:59:47 +0100

    Update create to avoid redirecting us to show post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,12 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to posts_path }
         format.json { render :show, status: :created, location: @post }
       else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@post, partial: 'posts/form', locals: { post: @post })
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
